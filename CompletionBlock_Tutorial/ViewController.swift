c9b15f0f3bcf3752e02147e002b38ddb29a38857
Hoonsbrand <63503972+Hoonsbrand@users.noreply.github.com>
2022-10-09 17:10:39 +0900

    completionBlock에 대한 간략한 공부

--- CompletionBlock_Tutorial/ViewController.swift
+++ CompletionBlock_Tutorial/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("ViewController - viewDidLoad() 호출됨")
+        
+        KRProgressHUD.show()
+        
+        sayHi(completion: { result in
+            print("completion block으로 넘겨받았음 : \(result)")
+            KRProgressHUD.showSuccess()
+            self.mainTitle.text = "Completion Block 완료"
+        })
     }
-
-
+    
+    fileprivate func sayHi(completion: @escaping (String) -> ()) {
+        
+        print("ViewController - sayHi() 호출됨")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            
+            completion("하이")
+        }
+    }
 }
 
